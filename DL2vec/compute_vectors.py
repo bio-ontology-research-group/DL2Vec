9fc2948cccc5a875027e228268a937f8905ea14a
garvinchen <839863272@qq.com>
2020-02-25 13:46:28 +0300

    Add files via upload

---
+++ DL2vec/compute_vectors.py
@@ -0,0 +1,108 @@
+from __future__ import print_function
+
+import numpy as np
+import random
+import json
+import sys
+import os
+import gensim
+
+import networkx as nx
+from networkx.readwrite import json_graph
+import multiprocessing as mp
+from threading import Lock
+import pickle as pkl
+
+
+lock = Lock()
+
+WALK_LEN=30
+N_WALKS=50
+
+global data_pairs
+data_pairs = []
+
+
+
+def run_random_walks(G, nodes, num_walks=N_WALKS):
+    print("now we start random walk")
+
+    pairs = []
+    for count, node in enumerate(nodes):
+
+        if G.degree(node) == 0:
+            continue
+        for i in range(num_walks):
+            curr_node = node
+            walk_accumulate=[]
+            for j in range(WALK_LEN):
+                next_node = random.choice(list(G.neighbors(curr_node)))
+
+                type_nodes = G.edges[curr_node, next_node]["type"]
+
+                if curr_node ==node:
+                    walk_accumulate.append(curr_node)
+                walk_accumulate.append(type_nodes)
+                walk_accumulate.append(next_node)
+
+                curr_node = next_node
+
+            pairs.append(walk_accumulate)
+        if count % 1000 == 0:
+            print("Done walks for", count, "nodes")
+    write_file(pairs)
+
+
+def run_walk(nodes,G):
+    global data_pairs
+
+    number=5
+    length = len(nodes) // number
+
+    processes = [mp.Process(target=run_random_walks, args=(G, nodes[(index) * length:(index + 1) * length])) for index
+                 in range(number-1)]
+    processes.append(mp.Process(target=run_random_walks, args=(G, nodes[(number-1) * length:len(nodes) - 1])))
+
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+
+
+
+    print("finish the work here")
+
+
+
+
+
+
+
+def write_file(pair):
+    with lock:
+        with open("data/walks.txt", "a") as fp:
+            for p in pair:
+                for sub_p in p:
+                    fp.write(str(sub_p)+" ")
+                fp.write("\n")
+
+
+def gene_node_vector(graph, entity_list,outfile):
+    nodes_set=set()
+    with open(entity_list,"r") as f:
+        for line in f.readlines():
+            data = line.strip().split()
+            for da in data:
+                nodes_set.add(da)
+
+
+    nodes_G= [n for n in graph.nodes()]
+
+    G = graph.subgraph(nodes_G)
+    nodes= [n for n in nodes_set]
+    run_walk(nodes,G)
+
+    print("start to train the word2vec models")
+    sentences=gensim.models.word2vec.LineSentence("data/walks.txt")
+    model=gensim.models.Word2Vec(sentences,sg=1, min_count=1, size=100, window=10,iter=30,workers=5)
+    model.save(outfile)
---
+++ DL2vec/runDL2vec.py
@@ -0,0 +1,64 @@
+import argparse
+import sys
+import os
+import networkx as nx
+import pickle as pkl
+import json
+from generate_graph import *
+from compute_vectors import *
+from networkx.readwrite import json_graph
+
+parser = argparse.ArgumentParser(description = "HOG2Vec: it is a tool can convert the complext axioms in the ontology into graph representation")
+
+parser.add_argument("-ontology", nargs = '?', metavar = "ontology OWL file", type = str,
+                     help = "File containing ontology in OWL format",default='')
+
+parser.add_argument("-associations", nargs="?",  metavar="annotation files ", type=str, help ="annotation files that link to the classes in the ontology")
+parser.add_argument("-outfile", nargs = '?', metavar = "output file", type = str,
+                     help = "Output file with ontology embeddings",default='')
+parser.add_argument("-embedsize", nargs = '?', metavar = "embedding size", type = int,default=100,
+                     help = "Size of obtained vectors")
+parser.add_argument("-windsize", nargs = '?', metavar = "window size", type = int,default=10,
+                     help = "Window size for word2vec model")
+parser.add_argument("-mincount", nargs = '?', metavar = "min count", type = int,default=1,
+                     help = "Minimum count value for word2vec model")
+parser.add_argument("-model", nargs = '?', metavar = "model", type = str,default='sg',
+                     help = "Preferred word2vec architecture, sg or cbow")
+
+parser.add_argument("-entity_list", nargs ="?", metavar ="the entity list that needs to generate the embedding", type=str, default="",
+                    help =" the entity list in which each entity that need to start random walk and generate the embedding")
+
+args = parser.parse_args()
+
+ontology_file =args.ontology
+association_file=args.associations
+window=args.windsize
+embedding=args.embedsize
+mincoun=args.mincount
+model=args.model
+outfile =args.outfile
+entity_list =args.entity_list
+
+if (ontology_file is '' ):
+	print ("\nError:Mandatory ontology file missing. For help, run: python runHOG2Vec.py --help\n")
+	sys.exit()
+if (association_file is ''):
+	print ("\nError:Mandatory association file missing. For help, run: python runHOG2Vec.py --help\n")
+	sys.exit()
+if (outfile is ''):
+	print ("\nError:Mandatory output-file name missing. For help, run: python runHOG2Vec.py --help\n")
+	sys.exit()
+
+if entity_list is "":
+    entity_list ==association_file
+
+if (model != 'sg' and model != 'cbow'):
+	model ='sg'
+commandF ="groovy ProcessOntology.groovy " + str(ontology_file) +" "+"elk"
+os.system(commandF)
+
+axiom_file = "axiomsorig.lst"
+G = generate_graph(association_file,axiom_file)
+
+
+gene_node_vector(G,entity_list,outfile)
